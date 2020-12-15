6d2713185787906f5dadc7dfcd670ebb6ddf9429
shangfucheng_glb <shangfucheng_glb@kgc.com>
2020-12-15 19:18:34 +0800

    1215作业

---
+++ kmall-user-web2/src/main/java/com/kgc/kmall/user/KmallUserWeb2Application.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallUserWeb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmallUserWeb2Application.class, args);
+    }
+
+}
