8e184260c16154ecc3aaf3e03487cb0884fbc012
shangfucheng_glb <shangfucheng_glb@kgc.com>
2020-12-16 19:46:55 +0800

    1216作业

---
+++ kmall-manager-web2/src/main/java/com/kgc/kmall/manager/KmallManagerWeb2Application.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallManagerWeb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmallManagerWeb2Application.class, args);
+    }
+
+}
